8a2fa79245f87c05606ac0897475dcd69e670eea
Sarah Drasner <sarah.drasner@gmail.com>
2019-09-28 16:33:12 -0600

    keep setting up the base example

---
+++ base-jamstack-sample/src/components/AppForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you're human:
+          <input name="bot-field" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email:
+          <input type="text" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
